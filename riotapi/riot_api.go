6e133f9fd4409acdb3950a07b422e6c43f384396
nmonterroso <nelsonmonterroso@gmail.com>
2016-04-30 10:04:47 -0700

    adding rito api generated code

--- riotapi/riot_api.go
+++ riotapi/riot_api.go
@@ -1,21 +1,32 @@
 package riotapi
 
-import "github.com/nmonterroso/lolchest.win/models"
+import (
+	runtime "github.com/go-openapi/runtime"
+	httptransport "github.com/go-openapi/runtime/client"
+	"github.com/nmonterroso/lolchest.win/models"
+	"github.com/nmonterroso/lolchest.win/riotapi/client"
+	"github.com/nmonterroso/lolchest.win/riotapi/client/operations"
+)
+
+const (
+	defaultRegion = "na"
+)
 
 type RiotApi interface {
 	GetChampionData() []*models.ChampionData
 }
 
 type riotAPI struct {
-	apiKey string
+	auth runtime.ClientAuthInfoWriter
 }
 
 func NewRiotApi(apiKey string) RiotApi {
 	return &riotAPI{
-		apiKey: apiKey,
+		auth: httptransport.APIKeyAuth("apiKey", "query", apiKey),
 	}
 }
 
 func (api *riotAPI) GetChampionData() []*models.ChampionData {
-
+	params := operations.NewGetChampionDataParams().WithRegion(defaultRegion)
+	data, err := client.Default.Operations.GetChampionData(params, api.auth)
 }
