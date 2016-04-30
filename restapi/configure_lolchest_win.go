2d09df06f11226faa38f840b12ad5d949f0f08a2
nmonterroso <nelsonmonterroso@gmail.com>
2016-04-30 08:06:57 -0700

    adding first pass at public api and generated files

---
+++ restapi/configure_lolchest_win.go
@@ -0,0 +1,55 @@
+package restapi
+
+import (
+	"crypto/tls"
+	"net/http"
+
+	errors "github.com/go-openapi/errors"
+	runtime "github.com/go-openapi/runtime"
+	middleware "github.com/go-openapi/runtime/middleware"
+
+	"github.com/nmonterroso/lolchest.win/restapi/operations"
+)
+
+// This file is safe to edit. Once it exists it will not be overwritten
+
+func configureFlags(api *operations.LolchestWinAPI) {
+	// api.CommandLineOptionsGroups = []swag.CommandLineOptionsGroup{ ... }
+}
+
+func configureAPI(api *operations.LolchestWinAPI) http.Handler {
+	// configure the api here
+	api.ServeError = errors.ServeError
+
+	api.JSONConsumer = runtime.JSONConsumer()
+
+	api.JSONProducer = runtime.JSONProducer()
+
+	api.GetAllChampDataHandler = operations.GetAllChampDataHandlerFunc(func() middleware.Responder {
+		return middleware.NotImplemented("operation .GetAllChampData has not yet been implemented")
+	})
+	api.GetSummonerHandler = operations.GetSummonerHandlerFunc(func(params operations.GetSummonerParams) middleware.Responder {
+		return middleware.NotImplemented("operation .GetSummoner has not yet been implemented")
+	})
+
+	api.ServerShutdown = func() {}
+
+	return setupGlobalMiddleware(api.Serve(setupMiddlewares))
+}
+
+// The TLS configuration before HTTPS server starts.
+func configureTLS(tlsConfig *tls.Config) {
+	// Make all necessary changes to the TLS configuration here.
+}
+
+// The middleware configuration is for the handler executors. These do not apply to the swagger.json document.
+// The middleware executes after routing but before authentication, binding and validation
+func setupMiddlewares(handler http.Handler) http.Handler {
+	return handler
+}
+
+// The middleware configuration happens before anything, this middleware also applies to serving the swagger.json document.
+// So this is a good place to plug in a panic handling middleware, logging and metrics
+func setupGlobalMiddleware(handler http.Handler) http.Handler {
+	return handler
+}
---
+++ restapi/doc.go
@@ -0,0 +1,22 @@
+/*Package restapi lolchest API
+
+API for lolchest
+
+
+    Schemes:
+      http
+    Host: api.lolchest.win
+    BasePath: /
+    Version: 1.0.0
+
+    Consumes:
+    - application/json
+
+
+    Produces:
+    - application/json
+
+
+swagger:meta
+*/
+package restapi
