2c434e9566a47d33b542ca59dd608219aaf25765
nguo <nina.x.guo@gmail.com>
2016-05-02 22:26:54 -0700

    startsWith filter

--- www/app/shared/filters.js
+++ www/app/shared/filters.js
@@ -1,12 +1,26 @@
 'use strict';
 
-/* Filters */
+/* Shared Filters */
 
 var lolFilters = angular.module('lolFilters', []);
 
-lolFilters.filter('checkmark', function() {
-
-	return function(input) {
-		return input ? '\u2713' : '\u2718';
-	};
-});
+lolFilters
+/**
+ * Filter a list for elements with start with the given string.
+ *
+ * @param {Array} items - The list of items to filter
+ * @param {String} prefix - The prefix to search for
+ * @param {String} [itemProperty] - An optional property to use for the search
+ *                                  if filtering a list of objects.
+ */
+	.filter('startsWith', function() {
+		return function(items, prefix, itemProperty) {
+			return items.filter(function(item) {
+				if (prefix === undefined) {
+					return true; // this is when the search prefix is blank
+				}
+				var findIn = itemProperty ? item[itemProperty] : item;
+				return findIn.toString().toLowerCase().indexOf(prefix.toLowerCase()) === 0;
+			});
+		};
+	});
