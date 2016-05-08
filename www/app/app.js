56a6cb62f6c4787aa71ac1f40ba4562d69cb9231
nmonterroso <nelsonmonterroso@gmail.com>
2016-05-07 19:13:53 -0700

    hella ghetto color management

--- www/app/app.js
+++ www/app/app.js
@@ -7,6 +7,7 @@ var lolApp = angular.module('lolApp', ['ngRoute', 'lolFilters', 'lolApp.profile'
 lolApp.config(['$locationProvider', '$routeProvider',
 	function($locationProvider, $routeProvider) {
 		$locationProvider.html5Mode(true);
+
 		$routeProvider
 			.when('/404', {
 				templateUrl: 'app/shared/404.html'
@@ -15,3 +16,17 @@ lolApp.config(['$locationProvider', '$routeProvider',
 				redirectTo: '/404'
 			});
 	}]);
+
+lolApp.controller('AppController', ['$rootScope', function($rootScope) {
+	$rootScope.$on('$routeChangeSuccess', function (event, currentRoute) {
+		console.log("ROUTE CHANGE", event, currentRoute);
+		switch(currentRoute.templateUrl) {
+			case 'app/modules/home/home.html':
+				$rootScope.bodyClass = 'home-view';
+				break;
+			case 'app/modules/profile/profile.html':
+				$rootScope.bodyClass = 'profile-view';
+				break;
+		}
+	});
+}]);
