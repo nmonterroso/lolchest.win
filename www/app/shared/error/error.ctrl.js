efc40d739c5cb20a0f07f15b108200a5f5dd0330
nguo <nina.x.guo@gmail.com>
2016-05-08 20:33:00 -0700

    display same region in selected dropdown as previously selected when page changes

--- www/app/shared/error/error.ctrl.js
+++ www/app/shared/error/error.ctrl.js
@@ -1,15 +1,19 @@
 'use strict';
 
 angular
-    .module('lolApp.error', ['ngResource'])
-    .controller('ErrorCtrl', [
-        '$location', '$scope',
-        function($location, $scope) {
-            $scope.message = "Uh Oh! An error occurred :(";
+  .module('lolApp.error', ['ngResource'])
+  .controller('ErrorCtrl', [
+    '$location', '$scope',
+    function($location, $scope) {
+      $scope.message = "Uh Oh! An error occurred :(";
+      var params = $location.search();
 
-            switch ($location.search().type) {
-                case "summoner_not_found":
-                    $scope.message = '"'+$location.search().name+'" was not found';
-                    break;
-            }
-        }]);
+      switch (params.type) {
+        case "summoner_not_found":
+          $scope.message = 'Summoner "' + params.name + '" was not found';
+          if (params.regionName) {
+            $scope.message += ' in ' + params.regionName;
+          }
+          break;
+      }
+    }]);
